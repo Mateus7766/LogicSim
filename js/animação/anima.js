925cdece76ffdcf19306e8a5e7be20ce8562dea3
KevindeLimaPB <kevindelimaalvespb@gmail.com>
2026-05-12 22:45:05 -0300

    Falta o Media da Main

--- js/animação/anima.js
+++ js/animação/anima.js
@@ -1,16 +1,22 @@
 // Lampada
 
-const lampada = document.querySelector('.lampada')
-const imagem = document.getElementById('imagem')
+const lampada = document.querySelector('.lampada');
+const imagem = document.getElementById('imagem');
+const main = document.getElementById('porta-bg');
+const lista = document.getElementById('porta-logicas-listas')
 
 function trocaImage(){
     const body = document.querySelector('body').classList.toggle("luz")
     
     if(imagem.src.includes("desligada.svg")){
         imagem.src = "./img/lampadas/ligada.svg"
+        main.classList.add("ativo");
+        lista.classList.add("hover");
     }else {
         imagem.src = "./img/lampadas/desligada.svg"
-    }
+         main.classList.remove("ativo");
+         lista.classList.remove("hover");    
+        }
 
 }
 
